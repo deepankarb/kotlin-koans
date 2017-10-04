67a59419d7dc8eb325d474de6add417f65332d79
Deepankar <deepankar@vitamap.com>
2017-10-04 16:09:24 +0530

    Solved part v.

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -1,7 +1,6 @@
 package v_builders
 
 import util.TODO
-import java.util.*
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -29,11 +28,17 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    //    todoTask37()
+    fun buildMap(f: HashMap<Int, String>.() -> Unit): HashMap<Int, String> {
+        val map: HashMap<Int, String> = HashMap<Int, String>()
+        map.f()
+        return map
+    }
+
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
